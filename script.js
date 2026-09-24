69efd1354caa52039421f70af3eec1704fda3253
Paul Barron <paulinobarron85@outlook.com>
2026-09-23 22:58:33 -0700

    reordering html, project rebooting

---
+++ script.js
@@ -0,0 +1,33 @@
+
+var latest = {};
+
+var projects = [
+    {
+        name: "name",
+        role: "my contributions",
+        status: "in-progress",
+        link: "google.com"
+    }
+];
+
+var selectedWorks = [
+    {
+        name: "song name",
+        link: "youtube.com",
+    }
+];
+
+var awards = [
+    {
+        name: "award name",
+        date: "awarded when"
+    }
+];
+
+var contacts = [
+    {
+        name: "where to find me",
+        contact: "website",
+    }
+];
+
